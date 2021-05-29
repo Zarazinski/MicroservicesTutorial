8463f88d7dd5c4bae0700312b0a06139dcba11ac
Sławomir Zaraziński <jestemKoderem@gmail.com>
2021-05-30 01:16:30 +0200

    Finish graphql

---
+++ api-gateway/src/graphql/resolvers/Query/listings.js
@@ -0,0 +1,11 @@
+const listingsResolver = async () => {
+    return [
+        {
+            description: "xd",
+            id: 1,
+            title: "test"
+        }
+    ];
+};
+
+export default listingsResolver;
