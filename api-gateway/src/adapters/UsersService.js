3882455a609b672845d0eaeec9cb390723d82e6b
Sławomir Zaraziński <jestemKoderem@gmail.com>
2021-06-05 02:12:51 +0200

    Finish part 5

--- api-gateway/src/adapters/UsersService.js
+++ api-gateway/src/adapters/UsersService.js
@@ -5,6 +5,13 @@ const USERS_SERVICE_URI = "http://users-service:7101";
 export default class UsersService {
     static async createUser({ email, password }) {
         const body = await got.post(`${USERS_SERVICE_URI}/users`, { json: { email, password } }).json()
+
+        return body;
+    }
+
+    static async createUserSession({ email, password }) {
+        const body = await got.post(`${USERS_SERVICE_URI}/sessions`, { json: { email, password } }).json()
+
         return body;
     }
 }
