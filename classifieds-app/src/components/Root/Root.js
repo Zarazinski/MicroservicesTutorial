998aca9e2a86c1f525be6bf53f09cb7a379ba969
Sławomir Zaraziński <jestemKoderem@gmail.com>
2021-06-14 00:34:02 +0200

    Part 7

--- classifieds-app/src/components/Root/Root.js
+++ classifieds-app/src/components/Root/Root.js
@@ -1,7 +1,10 @@
-import React from "react";
+import gql from 'graphql-tag';
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import Login from "./Login";
-
+import { useDispatch } from 'react-redux';
+import graphqlClient from "#root/api/graphqlClient";
+import { setSession } from '#root/store/ducks/session';
+import AccountDetails from './AccountDetails';
 
 const Wrapper = styled.div`
     box-sizing: border-box;
@@ -27,14 +30,41 @@ const Sidebar = styled.div`
     width: 10rem;
 `;
 
+const query = gql`
+    {
+        userSession(me: true) {
+            id
+            user {
+                email
+                id
+            }
+        }
+    }
+`;
+
 const Root = () => {
+    const dispatch = useDispatch();
+    const [initialised, setInitialised] = useState(false);
+
+    useEffect(() => {
+        graphqlClient.query({ query }).then(({ data }) => {
+            if (data.userSession) {
+                dispatch(setSession(data.userSession));
+            }
+
+            setInitialised(true);
+        });
+    }, []);
+
+    if (!initialised) return "Loading...";
+
     return <Wrapper>
         <Container>
             <Content>
                 Content
             </Content>
             <Sidebar>
-                <Login />
+                <AccountDetails />
             </Sidebar>
         </Container>
     </Wrapper>;
--- classifieds-app/src/theme.js
+++ classifieds-app/src/theme.js
@@ -1,1 +1,3 @@
 export const veryLightGreyt = "#CCCCCC";
+export const mortar = "#555555";
+export const nero = "#222222";
